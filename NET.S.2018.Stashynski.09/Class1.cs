7c4966cf84dc9956fd6af4807d9f74b9c5ca022d
Herman <hermanstashinskii@yandex.ru>
2018-07-13 14:18:21 +0300

    Add project files.

---
+++ NET.S.2018.Stashynski.09/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2018.Stashynski._09
+{
+    public class Class1
+    {
+    }
+}

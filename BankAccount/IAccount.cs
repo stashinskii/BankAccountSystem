a0833b7c14918a23dd3a2d916b7836504ab03e18
Herman <hermanstashinskii@yandex.ru>
2018-07-15 12:04:57 +0300

    Small fixes

--- BankAccount/IAccount.cs
+++ BankAccount/IAccount.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace BankAccount
 {
+    /// <summary>
+    /// Represents basic operations for implementation for account classes 
+    /// </summary>
     interface IAccount
     {
+        /// <summary>
+        /// Represents income operation
+        /// </summary>
+        /// <param name="amount">Amount of money</param>
         void Income(int amount);
+
+        /// <summary>
+        /// Represents outcome operation
+        /// </summary>
+        /// <param name="amount">Amount of money</param>
         void Outcome(int amount);
+
+        /// <summary>
+        /// Represents operation of closing an account
+        /// </summary>
         void CloseAccount();
     }
 }

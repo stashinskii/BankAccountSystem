c8d7b72a1a3d557d977d32362d4a68c23de6f4da
Herman <germanstashinskii@gmail.com>
2018-07-29 20:11:12 +0300

    Fake repositories added

--- AccountSystem/DAL.Fake/Repositories/HolderRepository.cs
+++ AccountSystem/DAL.Fake/Repositories/HolderRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL.Interface.DTO
+namespace DAL.Fake.Repositories
 {
-    interface IEntity
+    class HolderRepository
     {
     }
 }
